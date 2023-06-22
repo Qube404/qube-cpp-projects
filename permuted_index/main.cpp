131162188b8e98ba405f5a220aeddea6b01970b7
Qube404 <qube404@protonmail.com>
2023-06-23 00:12:08 +1000

    Finished rotating string function

--- permuted_index/main.cpp
+++ permuted_index/main.cpp
@@ -1,3 +1,16 @@
+#include <vector>
+#include <string>
+#include <iostream>
+
+#include "permuted_index.h"
+#include "split_string.h"
+
+using namespace std;
+
 int main() {
-    
+    vector<PermutedIndex> rotations = rotate_string("rotate this string please");
+
+    for (vector<string>::size_type i = 0; i != rotations.size(); i++) {
+        cout << rotations[i].line << endl;
+    }
 }
--- permuted_index/permuted_index.cpp
+++ permuted_index/permuted_index.cpp
@@ -11,41 +11,36 @@ using namespace std;
 typedef vector<string>::size_type vec_sz;
 typedef string::size_type str_sz;
 
-vector<PermutedIndex> rotate_string(const string& s) {
+vector<PermutedIndex> rotate_string(const string& str) {
+    string s = str + ' ';
     vector<PermutedIndex> ret;
-    vector<string> split_str = split(s);
 
     string working_str;
     PermutedIndex working_perm;
 
     // Push all rotations of a string into rotations
     // vector
-    for (vec_sz i = 0; i != split_str.size(); i++) {
-        working_str.clear();
+    for (vec_sz i = 0; i != s.size(); i++) {
+        if (s[i] == ' ') {
+            working_str.clear();
 
-        vec_sz start = i;
-        str_sz start_index = 0;
-        for (vec_sz j = 0; j != split_str.size(); j++) {
-            if (start == split_str.size()) {
-                start = 0;
-            } 
+            vec_sz start = i;
+            for (vec_sz j = 0; j != s.size(); j++) {
+                if (start == s.size()) {
+                    start = 0;
+                } 
 
-            working_str += split_str[start] + " ";
-            ++start;
+                working_str += s[start];
+                ++start;
+            }
+
+            string::size_type sz = working_str.size();
+            working_perm.line = working_str.substr(1, sz);
+            working_perm.start = 0;
+
+            ret.push_back(working_perm);
         }
-
-        working_perm.line = working_str;
-        working_perm.start = 0;
-        ret.push_back(working_perm);
     }
 
     return ret;
-}
-
-int main() {
-    vector<string> str = rotate_string("test read my balls");
-
-    for (vector<string>::size_type i = 0; i != str.size(); i++) {
-        cout << str[i] << endl;
-    }
 }
