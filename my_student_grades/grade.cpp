5cccc4c3c7a8586cb106d9ba6511fb10876eceb1
Qube404 <qube404@protonmail.com>
2023-06-25 10:09:41 +1000

    Got error handling structured comfortable with immediately calculated student info struct

--- my_student_grades/grade.cpp
+++ my_student_grades/grade.cpp
@@ -11,9 +11,5 @@ double grade(double midterm, double final, double homework) {
 }
 
 double grade(double midterm, double final, const vector<double>& hw) {
-    if (hw.size() == 0) {
-        throw domain_error("student has done no homework");
-    }
-
     return grade(midterm, final, median(hw));
 }
--- my_student_grades/main.cpp
+++ my_student_grades/main.cpp
@@ -16,28 +16,34 @@ int main() {
     StudentInfo record;
     string::size_type maxlen = 0;
 
-    while (read(cin, record)) {
+    string s;
+    stringstream ss;
+
+    while (getline(cin, s)) {
+        ss = stringstream(s);
+
+        try {
+            read(ss, record);
+        } catch (domain_error e) {
+            cout << e.what();
+            return -1;
+        }
+
         maxlen = max(maxlen, record.name.size());
         students.push_back(record);
     }
     
     sort(students.begin(), students.end(), compare);
     
+    cout << endl;
     for (vector<StudentInfo>::size_type i = 0;
          i != students.size(); i++) {
         cout << students[i].name
              << string(maxlen + 1 - students[i].name.size(), ' ');
 
-        try {
-            double final_grade = grade(students[i]);
-            streamsize prec = cout.precision();
-            cout << setprecision(3) << final_grade
-                 << setprecision(prec);
-        } catch (domain_error e) {
-            cout << e.what();
-        }
-
-        cout << endl;
+        streamsize prec = cout.precision();
+        cout << setprecision(3) << students[i].final_grade
+             << setprecision(prec) << endl;
     }
 
     return 0;
