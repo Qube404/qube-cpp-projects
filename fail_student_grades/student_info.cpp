a28fb288a1262dceb9db7e57622e0f4755ca8f72
Qube404 <qube404@protonmail.com>
2023-06-25 23:06:37 +1000

    Made failed grades version of student grades program

---
+++ fail_student_grades/student_info.cpp
@@ -0,0 +1,46 @@
+#include "student_info.h"
+#include "grade.h"
+
+using std::istream; using std::vector;
+
+bool compare(const StudentInfo& x, const StudentInfo& y) {
+    return x.name < y.name;
+}
+
+istream& read(istream& is, StudentInfo& s) {
+    is >> s.name >> s.midterm >> s.final;
+
+    read_hw(is, s.homework);
+    return is;
+}
+
+istream& read_hw(istream& in, vector<double>& hw) {
+    if (in) {
+        hw.clear();
+
+        double x;
+        while (in >> x) {
+            hw.push_back(x);
+        }
+
+        in.clear();
+    }
+
+    return in;
+}
+
+vector<StudentInfo> extract_fails(vector<StudentInfo>& s) {
+    vector<StudentInfo> fail;
+    vector<StudentInfo>::iterator iter = s.begin();
+
+    while (iter != s.end()) {
+        if (fgrade(*iter)) {
+            fail.push_back(*iter);
+            iter = s.erase(iter);
+        } else {
+            ++iter;
+        }
+    }
+
+    return fail;
+}
---
+++ fail_student_grades/student_info.h
@@ -0,0 +1,19 @@
+#ifndef GUARD_student_info
+#define GUARD_student_info
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+struct StudentInfo {
+    std::string name;
+    double midterm, final;
+    std::vector<double> homework;
+};
+
+bool compare(const StudentInfo&, const StudentInfo&);
+std::istream& read(std::istream&, StudentInfo&);
+std::istream& read_hw(std::istream&, std::vector<double>&);
+std::vector<StudentInfo> extract_fails(std::vector<StudentInfo>&);
+
+#endif
