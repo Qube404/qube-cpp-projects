be39941c1eaf473cee6d49aaf66bb33f2860996e
Qube404 <qube404@protonmail.com>
2023-06-28 10:02:48 +1000

    Fixed grammar divide by zero error

---
+++ grammar_rules/main.cpp
@@ -0,0 +1,95 @@
+#include <stdexcept>
+#include <vector>
+#include <string>
+#include <map>
+#include <iostream>
+
+#include "split.h"
+
+using namespace std;
+
+typedef vector<string> Rule;
+typedef vector<Rule> RuleCollection;
+typedef map<string, RuleCollection> Grammar;
+
+Grammar read_grammar(istream& in) {
+    Grammar ret;
+    string line;
+
+    while (getline(in, line)) {
+        vector<string> entry = split(line);
+        if (!entry.empty()) {
+            ret[entry[0]].push_back(
+                Rule(entry.begin() + 1, entry.end())
+            );
+        }
+    }
+
+    return ret;
+}
+
+int nrand(int n) {
+    if (n <= 0 || n > RAND_MAX) {
+        throw domain_error("argument to nrand is out of range");
+
+    // Avoiding divide by zero error that occurs when n is 1
+    } else if (n == 1) {
+        return 0;
+    }
+
+    const int bucket_size = RAND_MAX / n;
+
+    int r;
+    do {
+        r = rand() / bucket_size;
+    } while (r >= n || r <= 0);
+
+    return r;
+}
+
+bool bracketed(const string& s) {
+    return s.size() > 1 && s[0] == '<' && s[s.size() - 1] == '>';
+}
+
+void gen_aux(const Grammar& g, const string& word, vector<string>& ret) {
+    if (!bracketed(word)) {
+        ret.push_back(word);
+    } else {
+        Grammar::const_iterator it = g.find(word);
+        if (it == g.end()) {
+            throw logic_error("empty rule");
+        }
+
+        const RuleCollection& c = it->second;
+
+        const Rule& r = c[nrand(c.size())];
+
+        for (Rule::const_iterator i = r.begin(); i != r.end(); i++) {
+            gen_aux(g, *i, ret);
+        }
+    }
+}
+
+vector<string> gen_sentence(const Grammar& g) {
+    vector<string> ret;
+    gen_aux(g, "<sentence>", ret);
+    return ret;
+}
+
+int main() {
+    vector<string> sentence = gen_sentence(read_grammar(cin));
+
+    vector<string>::const_iterator it = sentence.begin();
+    if (!sentence.empty()) {
+        cout << *it;
+        ++it;
+    }
+
+    while (it != sentence.end()) {
+        cout << " " << *it;
+        ++it;
+    }
+
+    cout << endl;
+    return 0;
+}
