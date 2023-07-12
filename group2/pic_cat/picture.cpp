a930dd3436121740a34d4c241269625ce66dafe6
Qube404 <qube404@protonmail.com>
2023-07-13 05:09:54 +1000

    Finished copying down the picture classes

---
+++ group2/pic_cat/picture.cpp
@@ -0,0 +1,98 @@
+#include <string>
+
+#include "picture.h"
+
+using namespace std;
+
+void PicBase::pad(std::ostream& os, wd_sz beg, wd_sz end) {
+    while (beg != end) {
+        os << " ";
+        ++beg;
+    }
+}
+
+Picture frame(const Picture& pic) {
+    return new FramePic(pic.p);
+}
+
+Picture hcat(const Picture& l, const Picture& r) {
+    return new HCatPic(l.p, r.p);
+}
+
+Picture vcat(const Picture& t, const Picture& b) {
+    return new VCatPic(t.p, b.p);
+}
+
+Picture::Picture(const vector<string>& v): p(new StringPic(v)) {}
+
+std::ostream& operator<<(std::ostream& os, const Picture& picture) {
+    const PicBase::ht_sz ht = picture.p->height();
+    for (PicBase::ht_sz i = 0; i != ht; i++) {
+        picture.p->display(os, i, false);
+        os << endl;
+    }
+
+    return os;
+}
+
+PicBase::wd_sz StringPic::width() const {
+    PicBase::wd_sz n = 0;
+    for (PicBase::ht_sz i = 0; i != data.size(); ++i) {
+        n = max(n, data[i].size());
+    }
+
+    return n;
+}
+
+void StringPic::display(ostream& os, ht_sz row, bool do_pad) const {
+    wd_sz start = 0;
+
+    if (row < height()) {
+        os << data[row];
+        start = data[row].size();
+    }
+
+    if (do_pad) {
+        pad(os, start, width());
+    }
+}
+
+void VCatPic::display(ostream& os, ht_sz row, bool do_pad) const {
+    wd_sz w = 0;
+    if (row < top->height()) {
+        top->display(os, row, do_pad);
+        w = top->width();
+    } else if (row < height()) {
+        bottom->display(os, row - top->height(), do_pad);
+        w = bottom->width();
+    }
+
+    if (do_pad) {
+        pad(os, w, width());
+    }
+}
+
+void HCatPic::display(ostream& os, ht_sz row, bool do_pad) const {
+    left->display(os, row, do_pad || row < right->height());
+    right->display(os, row, do_pad);
+}
+
+void FramePic::display(ostream& os, ht_sz row, bool do_pad) const {
+    if (row >= height()) {
+        if (do_pad) {
+            pad(os, 0, width());
+        }
+    } else {
+        if (row == 0 || row == height() - 1) {
+            os << string(width(), '*');
+        } else if (row == 1 || row == height() - 2) {
+            os << '*';
+            pad(os, 1, width() - 1);
+            os << '*';
+        } else {
+            os << "* ";
+            p->display(os, row - 2, true);
+            os << " *";
+        }
+    }
+}
--- group2/pic_cat/picture.h
+++ group2/pic_cat/picture.h
@@ -1,68 +1,112 @@
+#ifndef GUARD_picture_h
+#define GUARD_picture_h
+
+#include <algorithm>
 #include <ostream>
 #include <vector>
 #include <string>
 
 #include "ptr.h"
 
+class Picture;
+
+// ---------------------------------------------------------------------- //
+
 class PicBase {
-protected:
+friend std::ostream& operator<<(std::ostream&, const Picture&);
+friend class FramePic;
+friend class HCatPic;
+friend class VCatPic;
+friend class StringPic;
+
+private:
     typedef std::vector<std::string>::size_type ht_sz;
     typedef std::string::size_type wd_sz;
 
-private:
     virtual wd_sz width() const = 0;
     virtual ht_sz height() const = 0;
-    virtual ht_sz disply(std::ostream&, ht_sz, bool) const = 0;
+    virtual void display(std::ostream&, ht_sz, bool) const = 0;
+
+protected:
+    static void pad(std::ostream& os, wd_sz beg, wd_sz end);
 };
 
+// ---------------------------------------------------------------------- //
+
+class Picture {
+friend Picture frame(const Picture&);
+friend Picture hcat(const Picture&, const Picture&);
+friend Picture vcat(const Picture&, const Picture&);
+friend std::ostream& operator<<(std::ostream&, const Picture&);
+
+public:
+    Picture(const std::vector<std::string>& = std::vector<std::string>());
+private:
+    Ptr<PicBase> p;
+    Picture(PicBase* ptr): p(ptr) {}
+};
+
+
+
+Picture frame(const Picture&);
+Picture hcat(const Picture&, const Picture&);
+Picture vcat(const Picture&, const Picture&);
+std::ostream& operator<<(std::ostream&, const Picture&);
+
+// ---------------------------------------------------------------------- //
+
 class StringPic: public PicBase {
+friend class Picture;
+
 private:
     std::vector<std::string> data;    
     StringPic(const std::vector<std::string>& v): data(v) {}
 
     wd_sz width() const;
-    ht_sz height() const;
+    ht_sz height() const { return data.size(); }
     void display(std::ostream&, ht_sz, bool) const;
 };
 
+// ---------------------------------------------------------------------- //
+
 class FramePic: public PicBase {
+friend Picture frame(const Picture&);
+
 private:
     Ptr<PicBase> p;
     FramePic(const Ptr<PicBase>& pic): p(pic) {}
 
-    wd_sz width() const;
-    ht_sz height() const;
+    wd_sz width() const { return p->width() + 4; }
+    ht_sz height() const { return p->height() + 4;}
     void display(std::ostream&, ht_sz, bool) const;
 };
 
+// ---------------------------------------------------------------------- //
+
 class VCatPic: public PicBase {
+friend Picture vcat(const Picture&, const Picture&);
+
 private:
     Ptr<PicBase> top, bottom;
     VCatPic(const Ptr<PicBase>& t, const Ptr<PicBase>& b): top(t), bottom(b) {}
 
-    wd_sz width() const;
-    ht_sz height() const;
+    wd_sz width() const { return std::max(top->width(), bottom->width()); }
+    ht_sz height() const { return top->height() + bottom->height(); }
     void display(std::ostream&, ht_sz, bool) const;
 };
 
+// ---------------------------------------------------------------------- //
+
 class HCatPic: public PicBase {
+friend Picture hcat(const Picture&, const Picture&);
+
 private:
     Ptr<PicBase> left, right;
     HCatPic(const Ptr<PicBase>& l, const Ptr<PicBase>& r): left(l), right(r) {}
 
-    wd_sz width() const;
-    ht_sz height() const;
+    wd_sz width() const { return left->width() + right->width(); }
+    ht_sz height() const { return std::max(left->height(), right->height()); }
     void display(std::ostream&, ht_sz, bool) const;
 };
 
-class Picture {
-public:
-    Picture(const std::vector<std::string>& = std::vector<std::string>());
-private:
-    Ptr<PicBase> p;
-};
-
-Picture frame(const Picture&);
-Picture hcat(const Picture&, const Picture&);
-Picture vcat(const Picture&, const Picture&);
-std::ostream operator<<(std::ostream&, const Picture&);
+#endif
