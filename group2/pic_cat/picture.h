87c9037d758dfcfc76cd2c7978b63e94784edff6
Qube404 <qube404@protonmail.com>
2023-07-13 00:09:30 +1000

    Finished structure of picture classes

--- group2/pic_cat/picture.h
+++ group2/pic_cat/picture.h
@@ -1,14 +1,59 @@
+#include <ostream>
 #include <vector>
 #include <string>
 
 #include "ptr.h"
 
-class PicBase {};
+class PicBase {
+protected:
+    typedef std::vector<std::string>::size_type ht_sz;
+    typedef std::string::size_type wd_sz;
 
-class StringPic: public PicBase {};
-class FramePic: public PicBase {};
-class VCatPic: public PicBase {};
-class HCatPic: public PicBase {};
+private:
+    virtual wd_sz width() const = 0;
+    virtual ht_sz height() const = 0;
+    virtual ht_sz disply(std::ostream&, ht_sz, bool) const = 0;
+};
+
+class StringPic: public PicBase {
+private:
+    std::vector<std::string> data;    
+    StringPic(const std::vector<std::string>& v): data(v) {}
+
+    wd_sz width() const;
+    ht_sz height() const;
+    void display(std::ostream&, ht_sz, bool) const;
+};
+
+class FramePic: public PicBase {
+private:
+    Ptr<PicBase> p;
+    FramePic(const Ptr<PicBase>& pic): p(pic) {}
+
+    wd_sz width() const;
+    ht_sz height() const;
+    void display(std::ostream&, ht_sz, bool) const;
+};
+
+class VCatPic: public PicBase {
+private:
+    Ptr<PicBase> top, bottom;
+    VCatPic(const Ptr<PicBase>& t, const Ptr<PicBase>& b): top(t), bottom(b) {}
+
+    wd_sz width() const;
+    ht_sz height() const;
+    void display(std::ostream&, ht_sz, bool) const;
+};
+
+class HCatPic: public PicBase {
+private:
+    Ptr<PicBase> left, right;
+    HCatPic(const Ptr<PicBase>& l, const Ptr<PicBase>& r): left(l), right(r) {}
+
+    wd_sz width() const;
+    ht_sz height() const;
+    void display(std::ostream&, ht_sz, bool) const;
+};
 
 class Picture {
 public:
@@ -16,3 +61,8 @@ public:
 private:
     Ptr<PicBase> p;
 };
+
+Picture frame(const Picture&);
+Picture hcat(const Picture&, const Picture&);
+Picture vcat(const Picture&, const Picture&);
+std::ostream operator<<(std::ostream&, const Picture&);
